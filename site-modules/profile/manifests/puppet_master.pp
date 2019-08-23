c6d8e737946b48c0722607db8f1480f6b499b133
Bill Claytor <35778171+billclaytor@users.noreply.github.com>
2019-08-23 11:31:07 -0700

    Updated collection_frequency to 1

--- site-modules/profile/manifests/puppet_master.pp
+++ site-modules/profile/manifests/puppet_master.pp
@@ -3,6 +3,7 @@ class profile::puppet_master {
   class { 'puppet_metrics_collector':
     puppetserver_hosts => [ $trusted['certname'] ],
     puppetdb_hosts     => [ $trusted['certname'] ],
+    collection_frequency => 1,
   }
 
 }
